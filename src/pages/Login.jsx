377b4bf611bd3ff351af9fa3538961987bb82c35
Alejandro <alejandro2003castrillon@gmail.com>
2024-11-03 20:32:55 -0500

    Commit del login, actualizado Web

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,97 +1,143 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import login from '../assets/login.jpeg';
 import logo from '../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 export default function Login() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleModalToggle = () => {
     setIsModalOpen(!isModalOpen);
   };
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/api/auth/login', {
+        username,
+        password
+      });
+      if (response.data.success) {
+        Swal.fire({
+          title: 'Registro exitoso',
+          text: 'Has iniciado sesión exitosamente.',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        }).then(() => {
+          navigate("/paginaPrincipal");
+        });
+      } else {
+        Swal.fire({
+          title: 'Error',
+          text: response.data.message,
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        title: 'Error',
+        text: 'Error en la autenticación. Intenta de nuevo.',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      });
+    }
+  };
 
   return (
-    <div className='flex min-h-screen'>
-
-      <div className='w-full md:w-1/2 flex flex-col justify-center items-center'>
-        <nav className='bg-slate-100 text-black p-4 w-full flex-shrink-0 items-center flex'>
-          <img src={logo} className='w-12 rounded-full mr-4' alt="Logo" />
-          <h1 className='text-3xl font-semibold'>EcoTuristea</h1>
-        </nav>
-
-        <div className='flex-1 flex items-center justify-center bg-green- p-8 w-full'>
-          <div className='max-w-md w-full'>
-            <h2 className='text-5xl font-bold text-gray-800 mb-6 text-center'>Iniciar Sesión</h2>
-            <p className='mb-8 text-slate-400 text-center text-xl'>Bienvenido, para continuar digita tus credenciales</p>
-            <form className='space-y-6'>
-              <div>
-                <label htmlFor="username" className='block text-gray-700 text-xl mb-2'>Usuario</label>
-                <input 
-                  type="text" 
-                  id="username" 
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="Ingresar usuario"
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className='block text-gray-700 text-xl mb-2'>Contraseña</label>
-                <input 
-                  type="password" 
-                  id="password" 
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="Ingresar contraseña"
-                  required
-                />
-              </div>
-              <p className='text-xs text-right cursor-pointer' onClick={handleModalToggle}>¿Olvidaste la contraseña?</p>
-              <button 
-                className='px-8 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition text-xl w-full h-14' 
-                type="submit" 
-                onClick={() => navigate("/paginaPrincipal")}
-              >
-                Iniciar Sesión
-              </button>
-              <div className='flex items-center justify-center'>
-                <p className='text-sm flex justify-center mr-1 text-gray-500 cursor-default'>¿No tienes cuenta?</p>
-                <p className='text-sm text-blue-900 cursor-pointer' onClick={() => navigate("/register")}>Regístrate</p>
-              </div>
-            </form>
+    <div className='flex flex-col min-h-screen'>
+      <nav className='bg-white shadow-md text-black p-4 w-full flex-shrink-0 flex items-center'>
+        <img src={logo} className='w-12 h-12 rounded-full mr-4' alt="Logo" />
+        <h1 className='text-3xl font-semibold text-green-600'>EcoTuristea</h1>
+      </nav>
+      <div className='flex flex-1 flex-col md:flex-row'>
+        <div className='w-full md:w-1/2 flex flex-col justify-center items-center bg-gray-100'>
+          <div className='flex-1 flex items-center justify-center p-8 w-full'>
+            <div className='max-w-md w-full'>
+              <h2 className='text-4xl font-bold text-green-800 mb-6 text-center'>Iniciar Sesión</h2>
+              <p className='mb-8 text-gray-600 text-center text-sm'>Bienvenido, para continuar digita tus credenciales</p>
+              <form className='space-y-6' onSubmit={handleLogin}>
+                <div>
+                  <label htmlFor="username" className='block text-gray-700 text-sm mb-2'>Usuario</label>
+                  <input
+                    type="text"
+                    id="username"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar usuario"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className='relative'>
+                  <label htmlFor="password" className='block text-gray-700 text-sm mb-2'>Contraseña</label>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <div className='absolute inset-y-0 right-0 pr-3 pt-7 flex items-center justify-center'>
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className='text-gray-600 focus:outline-none focus:text-gray-800'
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </button>
+                  </div>
+                </div>
+                <p className='text-xs text-blue-600 text-right cursor-pointer' onClick={handleModalToggle}>¿Olvidaste la contraseña?</p>
+                <button
+                  className='px-8 bg-green-600 text-white rounded-lg shadow-lg hover:bg-green-700 transition text-xl w-full h-14'
+                  type="submit"
+                >
+                  Iniciar Sesión
+                </button>
+                <div className='flex items-center justify-center mt-4'>
+                  <p className='text-sm text-gray-600'>¿No tienes cuenta?</p>
+                  <p className='text-sm text-blue-600 ml-1 cursor-pointer' onClick={() => navigate("/register")}>Regístrate</p>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
+        <div className='hidden lg:flex lg:w-1/2 justify-center items-center mr-12 bg-[#f3f4f6]'>
+          <img
+            src={login}
+            alt="Imagen descriptiva"
+            className='w-full h-3/4 object-cover'
+          />
+        </div>
       </div>
-
-
-      <div className='hidden md:hidden lg:block lg:w-1/2'>
-        <img 
-          src={login}
-          alt="Imagen descriptiva" 
-          className='w-full h-full object-cover '
-        />
-      </div>
-
-      {/* Modal para recuperar contraseña */}
       {isModalOpen && (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
           <div className='bg-white p-6 rounded-lg shadow-lg max-w-sm w-full'>
             <h2 className='text-xl font-bold mb-4'>Recuperar Contraseña</h2>
             <p className='mb-4'>Ingresa tu correo electrónico para recibir instrucciones sobre cómo recuperar tu contraseña.</p>
-            <input 
-              type="email" 
+            <input
+              type="email"
               className='w-full p-2 border border-gray-300 rounded-lg mb-4'
               placeholder="Correo electrónico"
             />
             <div className='flex justify-end'>
-              <button 
-                className='px-4 py-2 bg-blue-600 text-white rounded-lg'
+              <button
+                className='px-4 py-2 bg-green-600 text-white rounded-lg'
                 onClick={handleModalToggle}
               >
                 Enviar
               </button>
-              <button 
+              <button
                 className='ml-2 px-4 py-2 bg-gray-300 rounded-lg'
                 onClick={handleModalToggle}
               >
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import login from '../assets/login.jpeg';
-import logo from '../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import login from '../assets/login.jpeg';
 
-export default function Register() {
-  const navigate = useNavigate();
+const Register = () => {
   const [formData, setFormData] = useState({
     username: '',
     name: '',
@@ -14,30 +13,48 @@ export default function Register() {
     country: '',
     password: ''
   });
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value
-    });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // Validaciones
+    if (!formData.username || !formData.name || !formData.surname || !formData.country || !formData.password) {
+      Swal.fire({
+        title: 'Error',
+        text: 'Todos los campos son obligatorios.',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      });
+      return;
+    }
+    if (formData.password.length < 6) {
+      Swal.fire({
+        title: 'Error',
+        text: 'La contraseña debe tener al menos 6 caracteres.',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      });
+      return;
+    }
     try {
-      const response = await axios.post('/api/auth/register', formData);
+      const response = await axios.post('http://localhost:5000/api/auth/register', formData);
       Swal.fire({
         title: 'Registro exitoso',
         text: response.data.message,
         icon: 'success',
         confirmButtonText: 'Aceptar'
       }).then(() => {
-        navigate('/login');
+        navigate('/login'); // Redirigir a la página de login
       });
     } catch (error) {
       Swal.fire({
         title: 'Error',
-        text: error.response.data.message,
+        text: error.response?.data?.message || 'Ocurrió un error al registrar el usuario.',
         icon: 'error',
         confirmButtonText: 'Aceptar'
       });
@@ -45,94 +62,102 @@ export default function Register() {
   };
 
   return (
-    <div className='flex min-h-screen'>
-      <div className='w-1/2 flex flex-col'>
-        <nav className='bg-slate-100 text-black p-4 flex-shrink-0 flex items-center'>
-          <img src={logo} className='w-12 rounded-full mr-4' alt="Logo" />
-          <h1 className='text-3xl font-semibold'>EcoTuristea</h1>
-        </nav>
-
-        <div className='flex-1 flex items-center justify-center bg-green- p-8'>
-          <div className='max-w-md w-96'>
-            <h2 className='text-5xl font-bold text-gray-800 mb-6 text-center'>Regístrate</h2>
-            <p className='mb-8 text-slate-400 text-center text-xl'>Bienvenido, para continuar digita tus credenciales</p>
-            <form className='space-y-6' onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="username" className='block text-gray-700 text-xl mb-2'>Id Usuario</label>
-                <input
-                  type="text"
-                  id="username"
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="Ingresar Usuario"
-                  value={formData.username}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <label htmlFor="name" className='block text-gray-700 text-xl mb-2'>Nombre</label>
-                <input
-                  type="text"
-                  id="name"
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="Ingresar Nombre"
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <label htmlFor="surname" className='block text-gray-700 text-xl mb-2'>Apellido</label>
-                <input
-                  type="text"
-                  id="surname"
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="Ingresar Apellido"
-                  value={formData.surname}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <label htmlFor="country" className='block text-gray-700 text-xl mb-2'>País</label>
-                <input
-                  type="text"
-                  id="country"
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="Ingresar País"
-                  value={formData.country}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className='block text-gray-700 text-xl mb-2'>Contraseña</label>
-                <input
-                  type="password"
-                  id="password"
-                  className='w-full p-4 border border-gray-300 rounded-lg text-xl'
-                  placeholder="*********"
-                  value={formData.password}
-                  onChange={handleChange}
-                />
-              </div>
-              <button
-                className='px-8 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition text-xl w-full h-14'
-                type="submit"
-              >
-                Regístrate
-              </button>
-              <div className='flex items-center justify-center'>
-                <p className='text-sm flex justify-center mr-1 text-gray-500 cursor-default'>¿Ya tienes cuenta?</p>
-                <p className='text-sm text-blue-900 cursor-pointer' onClick={() => navigate("/login")}>Inicia sesión</p>
-              </div>
-            </form>
+    <div className='flex flex-col min-h-screen'>
+      <nav className='bg-white shadow-md text-black p-4 w-full flex-shrink-0 flex items-center'>
+        <img src={logo} className='w-12 h-12 rounded-full mr-4' alt="Logo" />
+        <h1 className='text-3xl font-semibold text-green-600'>EcoTuristea</h1>
+      </nav>
+      <div className='flex flex-1 flex-col md:flex-row'>
+        <div className='w-full md:w-1/2 flex flex-col justify-center items-center bg-gray-100'>
+          <div className='flex-1 flex items-center justify-center p-8 w-full'>
+            <div className='max-w-md w-full'>
+              <h2 className='text-4xl font-bold text-green-800 mb-6 text-center'>Regístrate</h2>
+              <p className='mb-8 text-gray-600 text-center text-sm'>Llena los siguientes campos para crear una cuenta</p>
+              <form className='space-y-6' onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="username" className='block text-gray-700 text-sm mb-2'>Usuario</label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar usuario"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="name" className='block text-gray-700 text-sm mb-2'>Nombre</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar nombre"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="surname" className='block text-gray-700 text-sm mb-2'>Apellido</label>
+                  <input
+                    type="text"
+                    id="surname"
+                    name="surname"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar apellido"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="country" className='block text-gray-700 text-sm mb-2'>País</label>
+                  <input
+                    type="text"
+                    id="country"
+                    name="country"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar país"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className='block text-gray-700 text-sm mb-2'>Contraseña</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className='w-full p-3 border border-gray-300 rounded-lg text-sm'
+                    placeholder="Ingresar contraseña"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <button
+                  className='px-8 bg-green-600 text-white rounded-lg shadow-lg hover:bg-green-700 transition text-xl w-full h-14'
+                  type="submit"
+                >
+                  Registrarse
+                </button>
+                <div className='flex items-center justify-center mt-4'>
+                  <p className='text-sm text-gray-600'>¿Ya tienes cuenta?</p>
+                  <p className='text-sm text-blue-600 ml-1 cursor-pointer' onClick={() => navigate("/login")}>Inicia sesión aquí</p>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='w-1/2'>
-        <img
-          src={login}
-          alt="Imagen descriptiva"
-          className='w-full h-full object-cover'
-        />
+        <div className='hidden lg:flex lg:w-1/2 justify-center items-center mr-12 bg-[#f3f4f6]'>
+          <img
+            src={login}
+            alt="Imagen descriptiva"
+            className='w-full h-3/4 object-cover'
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Register;
